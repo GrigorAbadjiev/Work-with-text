463a1166f42c5c00ceaae1d1520fc10620fa6bea
User <User@DESKTOP-ESP2JT1>
2022-11-08 15:17:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Work_with_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string name = "Grigor Abadjiev";
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
